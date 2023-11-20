6352ddd449e13a8050fc01ab4bc68aa7a7e4b93d
Pirzada Ahmad Faraz <136244856+pirzada-ahmadfaraz@users.noreply.github.com>
2023-11-21 01:15:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 const apiKey = "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";
 
-const maxImages = 4; // Number of images to generate for each prompt
+const maxImages = 4; 
 let selectedImageNumber = null;
 
-// Function to generate a random number between min and max (inclusive)
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Function to disable the generate button during processing
 function disableGenerateButton() {
     document.getElementById("generate").disabled = true;
 }
 
-// Function to enable the generate button after process
 function enableGenerateButton() {
     document.getElementById("generate").disabled = false;
 }
 
-// Function to clear image grid
 function clearImageGrid() {
     const imageGrid = document.getElementById("image-grid");
     imageGrid.innerHTML = "";
 }
 
-// Function to generate images
+
 async function generateImages(input) {
     disableGenerateButton();
     clearImageGrid();
@@ -35,10 +31,8 @@ async function generateImages(input) {
     const imageUrls = [];
 
     for (let i = 0; i < maxImages; i++) {
-        // Generate a random number between 1 and 10000 and append it to the prompt
         const randomNumber = getRandomNumber(1, 10000);
         const prompt = `${input} ${randomNumber}`;
-        // We added random number to prompt to create different results
         const response = await fetch(
             "https://api-inference.huggingface.co/models/prompthero/openjourney",
             {
@@ -69,7 +63,7 @@ async function generateImages(input) {
     loading.style.display = "none";
     enableGenerateButton();
 
-    selectedImageNumber = null; // Reset selected image number
+    selectedImageNumber = null; 
 }
 
 document.getElementById("generate").addEventListener('click', () => {
@@ -80,7 +74,6 @@ document.getElementById("generate").addEventListener('click', () => {
 function downloadImage(imgUrl, imageNumber) {
     const link = document.createElement("a");
     link.href = imgUrl;
-    // Set filename based on the selected image
     link.download = `image-${imageNumber + 1}.jpg`;
     link.click();
 }
